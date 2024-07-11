60d85232895ae8005922e5ce9c7e5131b3facaac
skatt931 <skatt931@gmail.com>
2024-07-11 16:00:20 +0200

    feat: add webpack cofiguration

--- src/Header.tsx
+++ src/Header.tsx
@@ -1,14 +1,10 @@
 import React from "react";
+import headerImg from "../assets/header_kitten.png";
 
 export default () => (
   <header>
     <div>
-      <img
-        src="https://www.catster.com/wp-content/uploads/2018/05/A-gray-cat-crying-looking-upset.jpg"
-        alt="Cat"
-        width="225"
-        id="head-cat"
-      />
+      <img src={headerImg} alt="Cat" width="225" id="head-cat" />
     </div>
     <h1>Welcome to Cat Cafe!</h1>
   </header>
---
+++ webpack.config.js
@@ -0,0 +1,43 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  entry: "./src/index.tsx",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js"
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: "ts-loader",
+        exclude: /node_modules/
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"]
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/,
+        type: "asset/resource"
+      }
+    ]
+  },
+  resolve: {
+    extensions: [".tsx", ".ts", ".js"]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./public/index.html"
+    })
+  ],
+  mode: "development",
+  devServer: {
+    static: {
+      directory: path.join(__dirname, "dist")
+    },
+    compress: true,
+    port: 3000
+  }
+};
